3ecb68a049128663299c8df093e72633d7a03731
julivansoares <julivan.soares@outlook.com>
2023-06-15 17:59:25 -0300

    metodoFilter

--- app/metodoFilter.js
+++ app/metodoFilter.js
@@ -10,9 +10,31 @@ botoes.forEach(btn=>btn.addEventListener('click',filtrarLivros)) // aqui eu faç
 function filtrarLivros() {
     const elementoBtn = document.getElementById(this.id); // this.id serve para indwentificar o botao clicado toda vez que a func é chamada ai guardo esse id
     const categoria =elementoBtn.value // agora pegor o valor desse id que é a categoria de cada botão ex(front, back,dados) salvo em uma variavel
-    const livrosFiltrados = categoria == 'disponivel' ? livros.filter(livro => livro.quantidade > 0) : livros.filter(livro => livro.categoria == categoria) // agora que tenho a categoria do livro clicado trago esse array com esses livros
-    console.log(categoria )
+    const livrosFiltrados = categoria == 'disponivel' ? filtrarPorDisponibilidade() : filtrarPorCategoria(categoria) // agora que tenho a categoria do livro clicado trago esse array com esses livros
+    console.log(categoria)
     exibirOsLivrosNaTela(livrosFiltrados)
      
+    if(categoria=='disponivel'){
+     const valorTotal = calcularValorTotalDeLivrosDisponiveis(livrosFiltrados)
+      exibirValorTotalDeLivrosDisponiveisNaTela(valorTotal)
+    }
 }
 
+
+function filtrarPorCategoria(categoria) {
+    return livros.filter(livro => livro.categoria == categoria);
+}
+
+function filtrarPorDisponibilidade() {
+    return livros.filter(livro => livro.quantidade > 0);
+}
+
+function exibirValorTotalDeLivrosDisponiveisNaTela(valorTotal){
+    valorTotalDeLivrosDisponivel.innerHTML=`
+    <section id="valor_total_livros_disponiveis">
+    <div class="livros__disponiveis">
+      <p>Todos os livros disponíveis por R$ <span id="valor">${valorTotal.toFixed(2)}</span></p>
+    </div>
+  </section>
+    `
+}
